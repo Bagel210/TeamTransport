1d27698812aeee8c343a9f6c72dea7b7d7f76625
Iain Brown <iainbrown210@gmail.com>
2015-02-03 15:49:41 +0000

    Added Flask Project for Comparison

---
+++ SeeEdin_Flask/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, flash, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome(name=None):
+    return render_template('Base.html', name=name)
+
+if __name__ == '__main__':
+	app.run()
