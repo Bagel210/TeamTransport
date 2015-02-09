da677aac6bc1a7aff89f561ba7d3fab01c72fddb
Iain Brown <iainbrown210@gmail.com>
2015-02-09 11:03:31 +0000

    Setting up Django to load up app

---
+++ SeeEdin/app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+# from django.contrib import admin
+# import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'SeeEdin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$','app.views.home')
+    # url(r'^/',include('app.urls')),
+)
