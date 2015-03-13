621f2ed0ca01b7e17873e1d1de08ce6dc651f44c
Iain Brown <iainbrown210@gmail.com>
2015-03-13 12:36:13 +0000

    Added Journey Planner for api

--- api.py
+++ api.py
@@ -13,33 +13,63 @@ stop_number = "36235979"
 BASE= "https://tfe-opendata.com/api/v1/"
 
 
-# @ http://tfe-opendata.readme.io/v1.0/docs/stops
 def get_list_of_stops():
+	"""
+	http://tfe-opendata.readme.io/v1.0/docs/stops
+	"""
 	r = requests.get(BASE+'stops')
 	
 	if r.status_code == 200:
 		return r.json()['stops']
 
 
-# @ http://tfe-opendata.readme.io/v1.0/docs/services
 def get_list_of_services():
-	GET /v1/services
+	"""
+	http://tfe-opendata.readme.io/v1.0/docs/services
+	"""
 	r = requests.get(BASE+'services')
 	
 	if r.status_code == 200:
 		return r.json()
 
 
-# @ http://tfe-opendata.readme.io/v1.0/docs/timetables
 def get_timetable_for_stop(stop_id):
+	"""
+	http://tfe-opendata.readme.io/v1.0/docs/timetables
+	"""
 	r = requests.get(BASE+'timetables/'+stop_id)
 
 	if r.status_code == 200:
 		return r.json()
 
 def time_table_between_stops(first_stop, last_stop, date, duration='120'):
-	# Note: must be on same journey
-	r = requests.get(BASE+'stoptostop-timetable/?start_stop_id='+first_stop+'&finish_stop_id='+last_stop+'&date='+date+'&duration='+duration)
+	"""
+	http://tfe-opendata.readme.io/v1.0/docs/stop-to-stop-timetables
+
+	first_stop and last_stop must be on same journey to use this method
+	"""
+
+	r = requests.get(BASE+'stoptostop-timetable/?start_stop_id=' + first_stop 
+							+ '&finish_stop_id=' + last_stop 
+							+ '&date=' + date 
+							+ '&duration=' + duration)
+
+	if r.status_code == 200:
+		return r.json()
+
+
+def journey_plan(begin_position, end_position, date, time_mode='LeaveAfter'):
+	"""
+	http://tfe-opendata.readme.io/v1.0/docs/journey-planner
+	
+	# Positions are tuples of float ->  (e.g. 55.31112,-3.12797)
+	# time mode can be either LeaveAfter or ArriveBy
+	"""
+
+	r = requests.get(BASE + '/directions/?start=' + begin_position 
+							+ '+&finish=' + end_position 
+							+ '&date=' + date 
+							+ '&time_mode=' + time_mode)
 
 	if r.status_code == 200:
 		return r.json()
