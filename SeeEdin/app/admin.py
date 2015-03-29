b003e651ace24bcad37f3385232e555388c8024a
nicolasanastassacos <nikkiea1094@googlemail.com>
2015-03-29 23:42:59 +0100

    added users to the admin
    Should be able to see the Users from the admin page now so they can be
    modified.

--- SeeEdin/app/admin.py
+++ SeeEdin/app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from app.models import Departures, BusStop, Attraction, Stops
+from app.models import Departures, BusStop, Attraction, Stops, UserProfile
 # Register your models here.
 
 admin.site.register(Departures)
 admin.site.register(BusStop)
 admin.site.register(Attraction)
 admin.site.register(Stops)
+admin.site.register(UserProfile)
